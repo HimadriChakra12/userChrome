0e88922b8eaaadc73aea9ae28cd15d2a3fde7654
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-07-31 19:21:20 +0600

    added desktop include

--- firefox.sh
+++ firefox.sh
@@ -3,6 +3,29 @@ curl "https://raw.githubusercontent.com/HimadriChakra12/.dotfiles/refs/heads/mas
 if [ ! -f /usr/lib/firefox/distribution/policies.json ]; then
     sudo cp $HOME/Downloads/policies.json /usr/lib/firefox/distribution/policies.json
 fi
+
+if [[ ! -f "$DESKTOP_FILE" ]]; then
+echo "Ensuring local .desktop entry exists..."
+mkdir -p ~/.local/share/applications
+
+DESKTOP_FILE="$HOME/.local/share/applications/firefox.desktop"
+
+    cat >"$DESKTOP_FILE" <<EOF
+[Desktop Entry]
+Name=Firefox
+Exec=firefox %u
+Type=Application
+Icon=firefox
+Terminal=false
+Categories=Network;WebBrowser;
+MimeType=x-scheme-handler/http;x-scheme-handler/https;
+StartupNotify=true
+EOF
+fi
+
+xdg-settings set default-web-browser firefox.desktop
+echo "Firefox set as default browser ✅"
+
 FIREFOX_DIR="$HOME/.config/mozilla/firefox"
 
 mkdir -p "$FIREFOX_DIR"
