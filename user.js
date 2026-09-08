3c02cc78c70cd72b5cf0b9186650ea6c99832550
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-09-08 20:16:47 +0600

    Peak

--- user.js
+++ user.js
@@ -182,3 +182,16 @@ user_pref("browser.sessionstore.interval", 30000);
 user_pref("network.http.max-connections-per-server", 8);
 
 user_pref("dom.ipc.processCount", 8);
+
+user_pref("general.smoothScroll", true);
+user_pref("general.smoothScroll.mouseWheel", true);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 400);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 150);
+
+user_pref("mousewheel.min_line_scroll_amount", 15);
+
+user_pref("apz.fling.enabled", true);
+user_pref("apz.overscroll.enabled", true);
+user_pref("layout.css.scroll-behavior.spring-constant", 0.5);
+user_pref("apz.fling_friction", 0.002);
+user_pref("apz.fling_stopped_threshold", 0.0);
